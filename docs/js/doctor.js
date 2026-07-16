1101e40ba196f5c6350cab6444d3b4fc5d910549
adamahmed-hcj <adamelseidy108@gmail.com>
2026-07-16 16:05:05 +0300

    .

--- docs/js/doctor.js
+++ docs/js/doctor.js
@@ -1,31 +1,30 @@
-const scriptURL = "";
+const scriptURL ="https://script.google.com/macros/s/AKfycbwJ-RUkcm5LERS8rZIn_bTZY7fGLD1P2D45Dt_YssYW22NmyjOG0VZW0O7RD1Zu3mav_w/exec";
 
 const buttons = document.querySelectorAll(".book");
 
 buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    
+    const doctorName = button.dataset.name;
 
-    button.addEventListener("click", () => {
+    if (!doctorName) {
+      alert("data-name");
+      return;
+    }
 
-        const doctorName = button.dataset.name;
-
-        fetch(scriptURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                doctor: doctorName
-            })
-        })
-        .then(response => response.text())
-        .then(() => {
-            alert("✅ " + doctorName + " booked successfully!");
-        })
-        .catch(error => {
-            console.error(error);
-            alert("❌ Booking failed.");
-        });
-
+   fetch(scriptURL, {
+  method: "POST",
+  body: JSON.stringify({
+    doctor: doctorName
+  })
+})
+    .then(response => response.text())
+    .then(() => {
+      alert("✅ " + doctorName + " booked successfully!");
+    })
+    .catch(error => {
+      console.error(error);
+      alert("❌ Booking failed.");
     });
-
+  });
 });
